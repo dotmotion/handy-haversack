f7f8482ae08c993716e61df426df8e2eabd5a99c
dotmotion <outon.x@gmail.com>
2020-01-19 11:56:16 -0600

    disabled menu

--- src/components/ui/AppDrawer.js
+++ src/components/ui/AppDrawer.js
@@ -37,7 +37,7 @@ export default function AppDrawer(props) {
             className={classes.menuButton}
             color="secondary"
             aria-label="menu"
-            onClick={e => props.drawer(e)}
+            // onClick={e => props.drawer(e)}
           >
             <MenuIcon />
           </IconButton>
