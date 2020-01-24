d2a21db3cd4a4c39c72545966ee9c33cf8886d8b
dotmotion <outon.x@gmail.com>
2020-01-24 11:55:32 -0600

    removed API calls and added a level filter to the class filter

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import { spells } from "../assets/db/spells";
 import D20 from "../assets/d20.svg";
-import { request, sortByClass, sortByLevel, sortBySchool } from "../utils/lib";
+import { sortByClass, sortByLevel, sortBySchool } from "../utils/lib";
 
 import SpellCard from "../components/ui/SpellCard";
 import FilterFab from "../components/ui/FilterFab";
@@ -90,12 +90,11 @@ class Search extends Component {
   };
 
   openModal = async spell => {
-    const res = await request(spell.url);
-    this.setState({ modal: true, selected: res });
+    this.setState({ modal: true, selected: spell });
   };
 
   setFilter = selection => {
-    this.setState({ filter: selection });
+    this.setState({ filter: selection, searchfield: "" });
   };
 
   closeModal = () => {
@@ -120,10 +119,15 @@ class Search extends Component {
         });
         break;
       case "class":
-        filteredSpells = {};
+        filteredSpells = spellsByClass;
         classList.forEach(c => {
-          filteredSpells[c] = spellsByClass[c].filter(spell => {
-            return spell.name.toLowerCase().includes(searchfield.toLowerCase());
+          const levels = Object.keys(c);
+          levels.forEach(l => {
+            filteredSpells[c][l] = spellsByClass[c][l].filter(spell => {
+              return spell.name
+                .toLowerCase()
+                .includes(searchfield.toLowerCase());
+            });
           });
         });
         break;
@@ -154,77 +158,87 @@ class Search extends Component {
           <div className="list">
             <FilterFab setFilter={this.setFilter} />
             <header className="search-header">
-              <TextField
-                placeholder="Search"
-                type="search"
-                fullWidth
-                InputProps={{
-                  className: classes.input,
-                  endAdornment: (
-                    <IconButton
-                      onClick={() => this.onSearchChange()}
-                      size="small"
-                    >
-                      {searchfield !== "" && (
-                        <ClearIcon style={{ color: "var(--txt2)" }} />
-                      )}
-                    </IconButton>
-                  )
-                }}
-                className={classes.root}
-                onChange={e => this.onSearchChange(e)}
-                value={searchfield}
-              />
+              {(filter === "spells" || filter === "school") && (
+                <TextField
+                  placeholder="Search"
+                  type="search"
+                  fullWidth
+                  InputProps={{
+                    className: classes.input,
+                    endAdornment: (
+                      <IconButton
+                        onClick={() => this.onSearchChange()}
+                        size="small"
+                        disableRipple={true}
+                      >
+                        <ClearIcon
+                          style={{
+                            color: "var(--txt2)",
+                            opacity: searchfield !== "" ? 1 : 0
+                          }}
+                        />
+                      </IconButton>
+                    )
+                  }}
+                  className={classes.root}
+                  onChange={e => this.onSearchChange(e)}
+                  value={searchfield}
+                />
+              )}
             </header>
-            {modal && (
-              <SpellCard
-                spell={selected}
-                open={modal}
-                close={this.closeModal}
-                trans={Transition}
-              />
-            )}
-            {filter === "spells" &&
-              filteredSpells.map(spell => (
-                <Paper
-                  elevation={5}
-                  className="list-paper"
-                  onClick={() => this.openModal(spell)}
-                  key={spell.name}
-                >
-                  {spell.name}
-                </Paper>
-              ))}
-
-            {filter === "class" &&
-              classList.map(c => (
-                <ExpPanel
-                  key={c}
-                  name={c}
-                  spells={filteredSpells[c]}
-                  onClick={this.openModal}
+            <div className="list-body">
+              {modal && (
+                <SpellCard
+                  spell={selected}
+                  open={modal}
+                  close={this.closeModal}
+                  trans={Transition}
                 />
-              ))}
+              )}
+              {filter === "spells" &&
+                filteredSpells.map(spell => (
+                  <Paper
+                    elevation={5}
+                    className="list-paper"
+                    onClick={() => this.openModal(spell)}
+                    key={spell.name}
+                  >
+                    {spell.name}
+                  </Paper>
+                ))}
 
-            {filter === "school" &&
-              schoolList.map(s => (
-                <ExpPanel
-                  key={s}
-                  name={s}
-                  spells={filteredSpells[s]}
-                  onClick={this.openModal}
-                />
-              ))}
+              {filter === "class" &&
+                classList.map(c => (
+                  <ExpPanel
+                    key={c}
+                    name={c}
+                    type={"class"}
+                    spells={filteredSpells[c]}
+                    onClick={this.openModal}
+                  />
+                ))}
 
-            {filter === "level" &&
-              [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(l => (
-                <ExpPanel
-                  key={l}
-                  name={l === 0 ? "Cantrips" : `Level ${l}`}
-                  spells={filteredSpells[l]}
-                  onClick={this.openModal}
-                />
-              ))}
+              {filter === "school" &&
+                schoolList.map(s => (
+                  <ExpPanel
+                    key={s}
+                    name={s}
+                    type={"school"}
+                    spells={filteredSpells[s]}
+                    onClick={this.openModal}
+                  />
+                ))}
+
+              {filter === "level" &&
+                [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(l => (
+                  <ExpPanel
+                    key={l}
+                    name={l === 0 ? "Cantrips" : `Level ${l}`}
+                    spells={filteredSpells[l]}
+                    onClick={this.openModal}
+                  />
+                ))}
+            </div>
           </div>
         ) : (
           <div className="wait">
