e3b46f5e1b6e24b48a111dcfe32441ef3eceae6e
dotmotion <outon.x@gmail.com>
2020-02-13 17:34:07 -0600

    Updates

---
+++ precache-manifest.843fe139254d0c9d33ab5e0bcf10a989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e132c345e45d981cc941e8720c4ac0f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de910b32b1a9f755c23",
+    "url": "/static/css/main.310b70ae.chunk.css"
+  },
+  {
+    "revision": "f472926e293e6f5be5eb",
+    "url": "/static/js/2.cd06b018.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/static/js/2.cd06b018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4de910b32b1a9f755c23",
+    "url": "/static/js/main.516f5d0a.chunk.js"
+  },
+  {
+    "revision": "bc870fca8cccd8be2f96",
+    "url": "/static/js/runtime-main.2dd28e8b.js"
+  },
+  {
+    "revision": "0b8d17b2b1f0abc2571cc56ac8b1e8cd",
+    "url": "/static/media/d20.0b8d17b2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/handy-haversack/precache-manifest.bd928a17e1c83aa239f8e552ac5716af.js"
+  "/precache-manifest.843fe139254d0c9d33ab5e0bcf10a989.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/handy-haversack/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
