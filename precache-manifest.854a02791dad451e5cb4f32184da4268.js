ede7a324110c00e71303020948f1dc2f77072d45
dotmotion <outon.x@gmail.com>
2020-02-19 12:56:54 -0600

    Updates

---
+++ precache-manifest.854a02791dad451e5cb4f32184da4268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb60f8784e8280f1c3488123b64c25da",
+    "url": "/handy-haversack/index.html"
+  },
+  {
+    "revision": "207fb85a329c17516101",
+    "url": "/handy-haversack/static/css/main.9afcddf4.chunk.css"
+  },
+  {
+    "revision": "f472926e293e6f5be5eb",
+    "url": "/handy-haversack/static/js/2.cd06b018.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "/handy-haversack/static/js/2.cd06b018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "207fb85a329c17516101",
+    "url": "/handy-haversack/static/js/main.900eb7df.chunk.js"
+  },
+  {
+    "revision": "404ead70db35be99d41d",
+    "url": "/handy-haversack/static/js/runtime-main.3cee07db.js"
+  },
+  {
+    "revision": "0b8d17b2b1f0abc2571cc56ac8b1e8cd",
+    "url": "/handy-haversack/static/media/d20.0b8d17b2.svg"
+  }
+]);
